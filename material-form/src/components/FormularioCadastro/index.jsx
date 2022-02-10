94c758fed45012923a7ca675558128b6ee2549d5
matheustakasaki <matheus8antunes@gmail.com>
2022-02-09 23:55:10 -0300

    Criando os outros textfields

--- material-form/src/components/FormularioCadastro/index.jsx
+++ material-form/src/components/FormularioCadastro/index.jsx
@@ -1,10 +1,14 @@
 import React from 'react';
-import { TextField } from '@mui/material'
+import { TextField, Button } from '@mui/material'
 
 function FormularioCadastro() {
     return (
         <>
-            <TextField id="outlined-basic" label="Outlined" variant="outlined" />
+            <TextField id="nome" label="Nome" variant="outlined" color="secondary" fullWidth/>
+            <TextField id="sobrenome" label="Sobrenome" variant="outlined" fullWidth/>
+            <TextField id="cpf" label="CPF" variant="outlined" fullWidth/>
+
+            <Button type="submit" variant="contained" color="primary">Cadastrar</Button>
         </>
     );
 }
