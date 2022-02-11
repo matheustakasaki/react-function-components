a631862cd31139cd9776cc96cd55122bf2535343
matheustakasaki <matheus8antunes@gmail.com>
2022-02-10 23:19:41 -0300

    Criando checkboxes(switches) ao formulario e componente de tipografia para títulos

--- material-form/src/components/FormularioCadastro/index.jsx
+++ material-form/src/components/FormularioCadastro/index.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { TextField, Button, Container } from '@mui/material'
+import { TextField, Button, Container, Switch, FormControlLabel, Typography } from '@mui/material'
 
 function FormularioCadastro() {
     return (
@@ -7,9 +7,14 @@ function FormularioCadastro() {
 
             <Container component="article" maxWidth="sm">
                 <h1>Formulário de Cadastro</h1>
+                <Typography variant="h3" component="h1" >Formulário de Cadastro</Typography>
                 <TextField id="nome" label="Nome" variant="outlined" color="secondary" fullWidth margin="normal" />
                 <TextField id="sobrenome" label="Sobrenome" variant="outlined" fullWidth margin="normal" />
                 <TextField id="cpf" label="cpf" variant="outlined" fullWidth margin="normal" />
+
+                <FormControlLabel label="Promoções" control={<Switch name="promocoes" defaultChecked={true} ></Switch>} />
+
+                <FormControlLabel label="Novidades" control={<Switch name="novidades" defaultChecked={true} ></Switch>} />
 
                 <Button type="submit" variant="contained" color="primary">Cadastrar</Button>
 
