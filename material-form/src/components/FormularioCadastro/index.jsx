a80c4e1aeaf0304cd5bd59fd1ec88ea466ca8e53
matheustakasaki <matheus8antunes@gmail.com>
2022-02-12 18:30:08 -0300

    Aprendendo state

--- material-form/src/components/FormularioCadastro/index.jsx
+++ material-form/src/components/FormularioCadastro/index.jsx
@@ -1,13 +1,17 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { TextField, Button, Container, Switch, FormControlLabel, Typography } from '@mui/material'
 
 function FormularioCadastro() {
+
+    let [nome, setNome] = useState('')
+
+    const arr = useState
     return (
         <>
 
             <Container component="article" maxWidth="sm">
                 <Typography variant="h3" component="h1" >Formulário de Cadastro</Typography>
-                <TextField id="nome" onChange={event => console.log(event.target.value)} label="Nome" variant="outlined" color="secondary" fullWidth margin="normal" />
+                <TextField id="nome" value={nome} onChange={event => setNome(event.target.value)} label="Nome" variant="outlined" color="secondary" fullWidth margin="normal" />
                 <TextField id="sobrenome" label="Sobrenome" variant="outlined" fullWidth margin="normal" />
                 <TextField id="cpf" label="cpf" variant="outlined" fullWidth margin="normal" />
 
