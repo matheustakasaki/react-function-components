563ed4785cd36ade52719d2f1092a4e99c795c0e
matheustakasaki <matheus8antunes@gmail.com>
2022-02-13 23:26:01 -0300

    Consertando erro de assincronicidade na atribuição de valor do input

--- material-form/src/components/FormularioCadastro/index.jsx
+++ material-form/src/components/FormularioCadastro/index.jsx
@@ -4,14 +4,21 @@ import { TextField, Button, Container, Switch, FormControlLabel, Typography } fr
 function FormularioCadastro() {
 
     let [nome, setNome] = useState('')
-
-    const arr = useState
     return (
         <>
 
             <Container component="article" maxWidth="sm">
                 <Typography variant="h3" component="h1" >Formulário de Cadastro</Typography>
-                <TextField id="nome" value={nome} onChange={event => setNome(event.target.value)} label="Nome" variant="outlined" color="secondary" fullWidth margin="normal" />
+                <TextField id="nome" value={nome} onChange={(event) => {
+                    let tmpNome = event.target.value;
+
+                    if (tmpNome.length >= 3) {
+                        tmpNome = setNome.tmpNome.substr(0, 3)
+
+                    }
+                    setNome(tmpNome)
+                }
+                } label="Nome" variant="outlined" color="secondary" fullWidth margin="normal" />
                 <TextField id="sobrenome" label="Sobrenome" variant="outlined" fullWidth margin="normal" />
                 <TextField id="cpf" label="cpf" variant="outlined" fullWidth margin="normal" />
 
