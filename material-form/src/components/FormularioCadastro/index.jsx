7d7c4b2844e12c7b36244b87613f0e2ef3ffaa71
matheustakasaki <matheus8antunes@gmail.com>
2022-02-13 23:41:20 -0300

    Adicionando estado ao restante dos campos do formulario

--- material-form/src/components/FormularioCadastro/index.jsx
+++ material-form/src/components/FormularioCadastro/index.jsx
@@ -4,29 +4,56 @@ import { TextField, Button, Container, Switch, FormControlLabel, Typography } fr
 function FormularioCadastro() {
 
     let [nome, setNome] = useState('')
+    let [sobrenome, setSobrenome] = useState('')
+    let [cpf, setCpf] = useState('')
+    let [promocoes, setPromocoes] = useState(true)
+    let [novidade, setNovidade] = useState(true)
     return (
         <>
 
             <Container component="article" maxWidth="sm">
                 <Typography variant="h3" component="h1" >Formulário de Cadastro</Typography>
-                <TextField id="nome" value={nome} onChange={(event) => {
-                    let tmpNome = event.target.value;
 
-                    if (tmpNome.length >= 3) {
-                        tmpNome = setNome.tmpNome.substr(0, 3)
+                <form onSubmit={(event) => {
+                    event.preventDefault();
 
+                    console.log()
+                }}>
+                    <TextField id="nome" value={nome} onChange={(event) => {
+                        let tmpNome = event.target.value;
+
+                        if (tmpNome.length >= 3) {
+                            tmpNome = setNome.tmpNome.substr(0, 3)
+
+                        }
+                        setNome(tmpNome)
                     }
-                    setNome(tmpNome)
-                }
-                } label="Nome" variant="outlined" color="secondary" fullWidth margin="normal" />
-                <TextField id="sobrenome" label="Sobrenome" variant="outlined" fullWidth margin="normal" />
-                <TextField id="cpf" label="cpf" variant="outlined" fullWidth margin="normal" />
+                    } label="Nome" variant="outlined" color="secondary" fullWidth margin="normal" />
 
-                <FormControlLabel label="Promoções" control={<Switch name="promocoes" defaultChecked={true} ></Switch>} />
+                    <TextField id="sobrenome" value={sobrenome} onChange={(event) => {
 
-                <FormControlLabel label="Novidades" control={<Switch name="novidades" defaultChecked={true} ></Switch>} />
+                        setSobrenome(event.target.value)
+                    }
+                    } label="Sobrenome" variant="outlined" fullWidth margin="normal" />
 
-                <Button type="submit" variant="contained" color="primary">Cadastrar</Button>
+                    <TextField id="cpf" value={cpf} onChange={(event) => {
+
+                        setCpf(event.target.value)
+                    }
+                    } label="cpf" variant="outlined" fullWidth margin="normal" />
+
+                    <FormControlLabel onChange={(event) => {
+                        setPromocoes(event.target.checked)
+                    }} label="Promoções" control={<Switch name="promocoes" defaultChecked={promocoes} ></Switch>} />
+
+                    <FormControlLabel onChange={(event) => {
+                        setNovidades(event.target.checked)
+                    }} label="Novidades" control={<Switch name="novidades" defaultChecked={novidades} ></Switch>} />
+
+                    <Button type="submit" variant="contained" color="primary">Cadastrar</Button>
+
+                </form>
+
 
             </Container>
         </>
