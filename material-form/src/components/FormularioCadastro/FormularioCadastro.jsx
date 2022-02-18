845f8a3eba583a9e5c33c7d1d0bfa02180a894ed
matheustakasaki <matheus8antunes@gmail.com>
2022-02-17 23:14:07 -0300

    Refatorando a divisão de etapas dos forms

--- material-form/src/components/FormularioCadastro/FormularioCadastro.jsx
+++ material-form/src/components/FormularioCadastro/FormularioCadastro.jsx
@@ -1,6 +1,5 @@
 import { Container, Typography } from '@mui/material';
-import React from 'react';
-import { useState } from 'react';
+import React, { useState } from 'react';
 import DadosEntrega from './DadosEntrega';
 import DadosPessoais from './DadosPessoais';
 import DadosUsuario from './DadosUsuario';
@@ -9,33 +8,22 @@ function FormularioCadastro({ aoEnviar, validarCpf }) {
     const [etapaAtual, setEtapaAtual] = useState(0)
 
 
+    const formularios = [
+        <DadosUsuario aoEnviar={proximo} />,
+        <DadosPessoais aoEnviar={proximo} validarCpf={validarCpf} />,
+        <DadosEntrega aoEnviar={aoEnviar} />
+    ]
+
     function proximo() {
         setEtapaAtual(etapaAtual + 1)
     }
-    function formularioAtual(etapa) {
-        switch (etapa) {
-            case 0:
-                return <DadosUsuario aoEnviar={proximo} />;
-            case 1:
-                return <DadosPessoais aoEnviar={aoEnviar} validarCpf={validarCpf} />
-            case 2:
-                return <DadosEntrega />
-            default:
-                return <Typography> Erro ao selecionar formulário</Typography>
-        }
-    }
+
     return (
         <>
-
             <Container component="article" maxWidth="sm">
                 <Typography variant="h3" component="h1" >Formulário de Cadastro</Typography>
-                {/* <DadosPessoais aoEnviar={aoEnviar} validarCpf={validarCpf} /> */}
-
-
-                {formularioAtual(etapaAtual)}
-                {/* <DadosUsuario /> */}
-
-                {/* <DadosEntrega /> */}
+                
+                { formularios[etapaAtual] }
 
             </Container>
         </>
