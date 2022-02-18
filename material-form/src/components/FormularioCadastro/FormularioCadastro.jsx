eadb7bd71be8eff05b5a9cef2540241830e10446
matheustakasaki <matheus8antunes@gmail.com>
2022-02-17 23:54:27 -0300

    Aprendendo a guardar/armazenar e mostrar dados utilizando o useEffect hook - nessa aula, aprendi como guardar estados entre componentes coisa muito importante para o react

--- material-form/src/components/FormularioCadastro/FormularioCadastro.jsx
+++ material-form/src/components/FormularioCadastro/FormularioCadastro.jsx
@@ -1,5 +1,5 @@
 import { Container, Typography } from '@mui/material';
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import DadosEntrega from './DadosEntrega';
 import DadosPessoais from './DadosPessoais';
 import DadosUsuario from './DadosUsuario';
@@ -7,12 +7,22 @@ import DadosUsuario from './DadosUsuario';
 function FormularioCadastro({ aoEnviar, validarCpf }) {
     const [etapaAtual, setEtapaAtual] = useState(0)
 
+    const [dadosColetados, setDados] = useState({})
 
+    useEffect(()=>{
+        console.log(dadosColetados);
+    })
     const formularios = [
-        <DadosUsuario aoEnviar={proximo} />,
-        <DadosPessoais aoEnviar={proximo} validarCpf={validarCpf} />,
-        <DadosEntrega aoEnviar={aoEnviar} />
+        <DadosUsuario aoEnviar={coletarDados} />,
+        <DadosPessoais aoEnviar={coletarDados} validarCpf={validarCpf} />,
+        <DadosEntrega aoEnviar={coletarDados} />
     ]
+
+    function coletarDados(dados){
+        setDados({...dadosColetados, ...dados})
+
+        proximo();
+    }
 
     function proximo() {
         setEtapaAtual(etapaAtual + 1)
